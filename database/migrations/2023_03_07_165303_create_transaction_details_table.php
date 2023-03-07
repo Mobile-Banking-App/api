b546f805e2d60523e3dd1007b0e38de06df27a09
oluchristian <oluchristian98@gmail.com>
2023-03-07 20:15:14 +0100

    feat: :construction: created models and migrations

---
+++ database/migrations/2023_03_07_165303_create_transaction_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('reference_number');
+            $table->string('account_name');
+            $table->string('account_number');
+            $table->string('bank_name');
+            $table->string('amount');
+            $table->enum('status', ['Successful', 'Failed', 'Pending']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_details');
+    }
+}
